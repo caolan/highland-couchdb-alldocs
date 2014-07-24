9c62931c5359d206b62dd8180611855891ce05b2
Caolan McMahon <caolan@caolanmcmahon.com>
2014-07-24 14:52:31 +0100

    refactor and add batch size option for getting docs using _all_docs with include_docs=true

--- index.js
+++ index.js
@@ -2,36 +2,43 @@ var _ = require('highland');
 var couchr = require('highland-couchr');
 
 
-exports.ids = function (dburl, batchsize) {
+function alldocs(dburl, batchsize, include_docs) {
     var curr;
     return _(function (push, next) {
-        var opt = {limit: batchsize};
+        var opt = {
+            limit: batchsize,
+            include_docs: true
+        };
         if (curr) {
             opt.start_key = JSON.stringify(curr);
             opt.skip = 1;
         }
-        var req = couchr.get(dburl + '/_all_docs', {limit: batchsize});
+        var req = couchr.get(dburl + '/_all_docs', opt);
         req.errors(push).apply(function (res) {
             var rows = res.body.rows;
+            curr = rows[rows.length - 1].id;
             rows.forEach(function (r) {
-                push(null, r.id);
+                push(null, r);
             });
             if (rows.length < batchsize) {
                 // no more ids
                 push(null, _.nil);
             }
             else {
-                curr = rows[rows.length - 1].id;
                 next();
             }
         });
     });
 };
 
-exports.docs = function (dburl, /*optional*/idbatch) {
-    var n = idbatch || 100;
-    return exports.ids(dburl, n).map(function (id) {
-        var url = dburl + '/' + encodeURIComponent(id);
-        return couchr.get(url, {}).pluck('body');
+exports.ids = function (dburl, batchsize) {
+    return alldocs(dburl, batchsize || 200, false).map(function (row) {
+        return row.id;
+    });
+};
+
+exports.docs = function (dburl, batchsize) {
+    return alldocs(dburl, batchsize || 20, true).map(function (row) {
+        return row.doc;
     });
 };
